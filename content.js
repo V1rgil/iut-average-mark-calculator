e7ffa1d0f8324af3d7519df002b9fa708e85c184
Virgil Rigagneau <virgil.rigagneau@etu.u-bordeaux.fr>
2026-03-28 13:59:23 +0100

    Refactor extension HTML structure and add manifest files for Chrome and Firefox

--- content.js
+++ content.js
@@ -25,125 +25,130 @@ async function main() {
     localStorage.setItem("siteUrl", siteUrl);
 
     initLocalStorage().then(async () => {
-        // Replaces the original page with the extension's page
-        document.open();
-        document.write(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href="${chrome.runtime.getURL('bootstrap/bootstrap.min.css')}" rel="stylesheet" crossorigin="anonymous">
-            <title>Notes</title>
-        </head>
-        <body data-bs-theme="dark" class="text-center">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <div class="navbar-brand">Relevé de notes</div>
-                <div class="d-flex gap-2">
-                    <div id="settings_container"></div>
-                    <a class="btn btn-danger" href="https://${siteUrl}/logout.php">Déconnexion</a>
+        // Create the HTML structure of the extension.
+        const extensionHtml = `
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="${chrome.runtime.getURL('bootstrap/bootstrap.min.css')}" rel="stylesheet">
+                <title>Notes</title>
+            </head>
+            <body data-bs-theme="dark" class="text-center">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <div class="navbar-brand">Relevé de notes</div>
+                    <div class="d-flex gap-2">
+                        <div id="settings_container"></div>
+                        <a class="btn btn-danger" href="https://${siteUrl}/logout.php">Déconnexion</a>
+                    </div>
                 </div>
-            </div>
-            <div id="loading" class="spinner-grow" style="position:absolute; top: 1rem; left: 49%;" role="status"></div>
-            </nav>
+                <div id="loading" class="spinner-grow" style="position:absolute; top: 1rem; left: 49%;" role="status"></div>
+                </nav>
 
-            <div id="popup-bg" class="position-fixed top-50 start-50 translate-middle rounded-md shadow-sm w-100 h-100" style="background-color: rgba(20, 20, 20, 0.5); backdrop-filter: blur(4px); display: none; z-index: 124;">
-                <div id="popup-content" class="d-flex flex-column gap-2 position-absolute top-50 start-50 translate-middle p-4 rounded z-2" style="background-color: rgb(40, 40, 40);">
-                    <div>
-                        <h4 id="notesrepar-title"></h4>
-                        <div id="notesrepar-description" class="text-secondary"></div>
+                <div id="popup-bg" class="position-fixed top-50 start-50 translate-middle rounded-md shadow-sm w-100 h-100" style="background-color: rgba(20, 20, 20, 0.5); backdrop-filter: blur(4px); display: none; z-index: 124;">
+                    <div id="popup-content" class="d-flex flex-column gap-2 position-absolute top-50 start-50 translate-middle p-4 rounded z-2" style="background-color: rgb(40, 40, 40);">
+                        <div>
+                            <h4 id="notesrepar-title"></h4>
+                            <div id="notesrepar-description" class="text-secondary"></div>
+                        </div>
+                        
+                        <div id="notesrepar-data" class="d-flex flex-row gap-2 align-items-end">
+                            <div style="width: 1rem; height: 7rem; background-color: #7171f4;"></div>
+                            <div style="width: 1rem; height: 11rem; background-color: #7171f4;"></div>
+                        </div>
+                        <div id="notesrepar-index" class="d-flex flex-row gap-2">
+                            <div style="width: 1rem;">0</div>
+                            <div style="width: 1rem;">1</div>
+                        </div>
+
+                        <div class="d-flex justify-content-center d-flex flex-column mx-auto" style="width: 15rem">
+                            <div class="d-flex justify-content-center d-flex flex-column mx-auto" style="width: 7rem">
+                                <div class="text-white d-flex flex-row justify-content-between"><div>Note : </div><div id="notesrepar-note"></div></div>
+                                <div class="text-white d-flex flex-row justify-content-between"><div>Rang : </div><div id="notesrepar-position"></div></div>
+                            </div>
+                            <div class="d-flex flex-row mt-1 align-items-center" style="height: 1rem">
+                                <div class="me-1">1er</div>
+                                <div id="notesrepar-range-superior" style="height: 1rem; background-color: rgb(25, 200, 200)"></div>
+                                <div id="notesrepar-range-current" style="height: 1rem; background-color: rgb(25, 135, 84)"></div>
+                                <div id="notesrepar-range-inferior" style="height: 1rem; background-color: rgb(25, 200, 200)"></div>
+                                <div class="ms-1">Dernier</div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="d-flex gap-2 m-auto mb-2 mt-2 justify-content-center gap-2">
+                                <div title="Minimum promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-danger">Min</span><div id="notesrepar-min"> --.--</div></div>
+                                <div>
+                                    <div title="Mediane promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-secondary">Med</span><div id="notesrepar-med"> --.--</div></div>
+                                    <div title="Moyenne promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-secondary">Moy</span><div id="notesrepar-moy"> --.--</div></div>
+                                </div>
+                                <div title="Maximum promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-success">Max</span><div id="notesrepar-max"> --.--</div></div>
+                            </div>
+                            <div id="notesrepar-coef">0</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="semestres btn-group m-4" role="group"></div>
+
+                <div class="card text-start m-auto" style="width: 90%; max-width: 60rem; min-width: 35rem;">
+                <h2 id="semester_title" class="card-title text-center mt-2">Semestre - - ----</h2>    
+                    <div class="d-flex flex-column m-auto align-items-center" style="width: 250px;">
+                    <div class="fs-5 d-flex justify-content-between" style="width: 200px;"><div>Moyenne :</div><div id="average">--.--</div></div>
+                    <div class="fs-5 d-flex justify-content-between" style="width: 200px;"><div>Rang :</div><div id="rank">--/--</div></div>
+                    <div class="fs-5 d-flex justify-content-between" style="width: 200px;"><div>ECTS:</div><div id="ects">--/--</div></div>
                     </div>
                     
-                    <div id="notesrepar-data" class="d-flex flex-row gap-2 align-items-end">
-                        <div style="width: 1rem; height: 7rem; background-color: #7171f4;"></div>
-                        <div style="width: 1rem; height: 11rem; background-color: #7171f4;"></div>
-                    </div>
-                    <div id="notesrepar-index" class="d-flex flex-row gap-2">
-                        <div style="width: 1rem;">0</div>
-                        <div style="width: 1rem;">1</div>
-                    </div>
-
-                    <div class="d-flex justify-content-center d-flex flex-column mx-auto" style="width: 15rem">
-                        <div class="d-flex justify-content-center d-flex flex-column mx-auto" style="width: 7rem">
-                            <div class="text-white d-flex flex-row justify-content-between"><div>Note : </div><div id="notesrepar-note"></div></div>
-                            <div class="text-white d-flex flex-row justify-content-between"><div>Rang : </div><div id="notesrepar-position"></div></div>
-                        </div>
-                        <div class="d-flex flex-row mt-1 align-items-center" style="height: 1rem">
-                            <div class="me-1">1er</div>
-                            <div id="notesrepar-range-superior" style="height: 1rem; background-color: rgb(25, 200, 200)"></div>
-                            <div id="notesrepar-range-current" style="height: 1rem; background-color: rgb(25, 135, 84)"></div>
-                            <div id="notesrepar-range-inferior" style="height: 1rem; background-color: rgb(25, 200, 200)"></div>
-                            <div class="ms-1">Dernier</div>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div class="d-flex gap-2 m-auto mb-2 mt-2 justify-content-center gap-2">
-                            <div title="Minimum promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-danger">Min</span><div id="notesrepar-min"> --.--</div></div>
-                            <div>
-                                <div title="Mediane promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-secondary">Med</span><div id="notesrepar-med"> --.--</div></div>
-                                <div title="Moyenne promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-secondary">Moy</span><div id="notesrepar-moy"> --.--</div></div>
-                            </div>
-                            <div title="Maximum promo" class="d-flex flex-row align-items-center gap-1"><span class="badge bg-success">Max</span><div id="notesrepar-max"> --.--</div></div>
-                        </div>
-                        <div id="notesrepar-coef">0</div>
+                    <div class="d-flex gap-2 m-auto mb-2 mt-2">
+                    <div title="Minimum promo" id="minPromo"><span class="badge bg-danger">Min</span> --.--</div>
+                    <div title="Moyenne promo" id="moyPromo"><span class="badge bg-secondary">Moy</span> --.--</div>
+                    <div title="Maximum promo" id="maxPromo"><span class="badge bg-success">Max</span> --.--</div>
                     </div>
                 </div>
-            </div>
 
-            <div class="semestres btn-group m-4" role="group"></div>
-
-            <div class="card text-start m-auto" style="width: 90%; max-width: 60rem; min-width: 35rem;">
-            <h2 id="semester_title" class="card-title text-center mt-2">Semestre - - ----</h2>    
-                <div class="d-flex flex-column m-auto align-items-center" style="width: 250px;">
-                <div class="fs-5 d-flex justify-content-between" style="width: 200px;"><div>Moyenne :</div><div id="average">--.--</div></div>
-                <div class="fs-5 d-flex justify-content-between" style="width: 200px;"><div>Rang :</div><div id="rank">--/--</div></div>
-                <div class="fs-5 d-flex justify-content-between" style="width: 200px;"><div>ECTS:</div><div id="ects">--/--</div></div>
+                <div id="ressources-container" class="card text-start m-auto mt-2 p-4" style="width: 90%; max-width: 60rem; min-width: 35rem;">
+                <h2 class="text-center mb-2">Ressources</h2>
+                <div id="ressources" class="d-flex flex-column gap-2">
                 </div>
+                </div>
+
+                <div id="saes-container" class="card text-start m-auto mt-2 p-4" style="width: 90%; max-width: 60rem; min-width: 35rem;">
+                <h2 class="text-center mb-2">SAÉs</h2>
+                <div id="saes" class="d-flex flex-column gap-2">
+                </div>
+                </div>
+
+                <div id="ues-container" class="card text-start m-auto mt-2 p-4" style="width: 90%; max-width: 60rem; min-width: 35rem;">
+                    <h2 class="text-center mb-2">UEs</h2>
+                    <div id="ues" class="d-flex flex-column gap-2">
+                    </div>
+                </div>
+
+                <footer class="p-4">
+                <p>
+                    <i>Cette extension présente les notes universitaires à partir du site officiel de l'université avec <br>
+                    un nouveau design et un calcul des moyennes tout en maintenant les données d'origine. Néanmoins, <br>
+                    l'extension décline toute responsabilité en cas d'éventuels problèmes d'affichage ou de calcul des <br>
+                    moyennes. Vous pouvez vérifier le code source par vous-même via le lien présent ci-dessous.</i>
+                </p>
                 
-                <div class="d-flex gap-2 m-auto mb-2 mt-2">
-                <div title="Minimum promo" id="minPromo"><span class="badge bg-danger">Min</span> --.--</div>
-                <div title="Moyenne promo" id="moyPromo"><span class="badge bg-secondary">Moy</span> --.--</div>
-                <div title="Maximum promo" id="maxPromo"><span class="badge bg-success">Max</span> --.--</div>
-                </div>
-            </div>
+                <a class="m-4" href="https://github.com/RedoverPlayer/average_calculator" target="_blank">https://github.com/RedoverPlayer/average_calculator</a>
+                </footer>
 
-            <div id="ressources-container" class="card text-start m-auto mt-2 p-4" style="width: 90%; max-width: 60rem; min-width: 35rem;">
-            <h2 class="text-center mb-2">Ressources</h2>
-            <div id="ressources" class="d-flex flex-column gap-2">
-            </div>
-            </div>
+                <div class="wait" style="display: none;"></div>
+                <div class="auth" style="display: none;"></div>
+            </body>
+        `;
 
-            <div id="saes-container" class="card text-start m-auto mt-2 p-4" style="width: 90%; max-width: 60rem; min-width: 35rem;">
-            <h2 class="text-center mb-2">SAÉs</h2>
-            <div id="saes" class="d-flex flex-column gap-2">
-            </div>
-            </div>
+        // Parse the extension HTML string to a page
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(extensionHtml, 'text/html');
 
-            <div id="ues-container" class="card text-start m-auto mt-2 p-4" style="width: 90%; max-width: 60rem; min-width: 35rem;">
-                <h2 class="text-center mb-2">UEs</h2>
-                <div id="ues" class="d-flex flex-column gap-2">
-                </div>
-            </div>
-
-            <footer class="p-4">
-            <p>
-                <i>Cette extension présente les notes universitaires à partir du site officiel de l'université avec <br>
-                un nouveau design et un calcul des moyennes tout en maintenant les données d'origine. Néanmoins, <br>
-                l'extension décline toute responsabilité en cas d'éventuels problèmes d'affichage ou de calcul des <br>
-                moyennes. Vous pouvez vérifier le code source par vous-même via le lien présent ci-dessous.</i>
-            </p>
-            
-            <a class="m-4" href="https://github.com/RedoverPlayer/average_calculator" target="_blank">https://github.com/RedoverPlayer/average_calculator</a>
-            </footer>
-
-            <div class="wait" style="display: none;"></div>
-            <div class="auth" style="display: none;"></div>
-        </body>
-        </html>
-        `);
-        document.close();
+        // Replace the entire page with the extension's page
+        document.replaceChild(
+            document.importNode(doc.documentElement, true),
+            document.documentElement
+        );
 
         // Set theme
         chrome.storage.sync.get('theme').then(data => document.body.setAttribute('data-bs-theme', data.theme));
